956127e79d74352ba5047e2ef785b0071b9c5935
Alexandre-st <alexandre.saintprix@gmail.com>
2023-11-10 16:01:48 +0100

    Add new structure for browserRouter

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Hello</li>
+          <li>Bonjour</li>
+          <li>Hola</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+ 
+export default Header;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,7 @@
+const Homepage = () => {
+  return (
+    <h1 style={{backgroundColor: "red"}}>Hello World !!</h1>
+  );
+}
+ 
+export default Homepage;
